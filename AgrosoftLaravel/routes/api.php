0dbc9cef5f091aede49eb5448f990e53c80da223
maiberCordoba <cordobamaiber@gmail.com>
2025-06-25 21:14:27 -0500

    lotes

--- AgrosoftLaravel/routes/api.php
+++ AgrosoftLaravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LotesController;
 use App\Http\Controllers\UsuariosController;
 
 Route::prefix('usuarios')->group(function () {
@@ -17,4 +18,20 @@ Route::prefix('usuarios')->group(function () {
         Route::patch('/{identificacion}', [UsuariosController::class, 'update']);
         Route::get('/me', [UsuariosController::class, 'getCurrentUser']);
     });
+});
+
+Route::prefix('lotes')->middleware('jwt.verify')->group(function () {
+    // CRUD 
+    Route::get('/', [LotesController::class, 'index']);
+    Route::get('/{id}', [LotesController::class, 'show']);
+    Route::post('/', [LotesController::class, 'store']);
+    Route::patch('/{id}', [LotesController::class, 'update']);
+    Route::delete('/{id}', [LotesController::class, 'destroy']);
+
+    // Búsquedas específicas
+    Route::get('/ubicacion/{posX}/{posY}', [LotesController::class, 'getByUbicacion']);
+    Route::get('/estado/{estado}', [LotesController::class, 'getByEstado']);
+
+    // Reportes
+    Route::get('/reporte/dimensiones/{tamX}/{tamY}', [LotesController::class, 'getByDimensiones']);
 });
