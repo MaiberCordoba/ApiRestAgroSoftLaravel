9e547e81996b019480ad18c395c5560fb12fb660
advg-2005 <advguwu.09.02.2005@gmail.com>
2025-06-26 12:03:51 -0500

    Creo los modelos para las tablas de finanzas

---
+++ AgrosoftLaravel/app/Models/Desechos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Desechos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fk_Cultivos',
+        'fk_TiposDesechos',
+        'nombre',
+        'descripcion',
+    ];
+}
